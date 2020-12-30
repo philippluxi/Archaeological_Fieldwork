5255bade8ef871af3fecc90abc1bed5f70ca7838
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2020-12-30 01:07:44 +0100

    [Lab 01] 02 - Introduced Spot Activity Design and refactored corresponding activity

--- app/src/main/java/com/archaeologicalfieldwork/SpotActivity.kt
+++ app/src/main/java/com/archaeologicalfieldwork/SpotActivity.kt
@@ -2,10 +2,13 @@ package com.archaeologicalfieldwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class SpotActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    info("Spot Activity started..")
+    setContentView(R.layout.activity_spot)
   }
 }
