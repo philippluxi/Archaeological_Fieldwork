dd365b8add9e36bc89d1bef2c35b72449eb1e64c
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2021-01-01 20:59:01 +0100

    [Lab 07] 04 - Initialize Map in Mapview

--- app/src/main/java/com/archaeologicalfieldwork/activities/SpotMapsActivity.kt
+++ app/src/main/java/com/archaeologicalfieldwork/activities/SpotMapsActivity.kt
@@ -1,16 +1,47 @@
 package com.archaeologicalfieldwork.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import com.archaeologicalfieldwork.R
-import kotlinx.android.synthetic.main.activity_spot_list.*
+
+import kotlinx.android.synthetic.main.activity_spot_maps.*
+
 
 class SpotMapsActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spot_maps)
-
         setSupportActionBar(toolbar)
-        toolbar.title = title
+        mapView.onCreate(savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
