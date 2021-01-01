12780a4a1b99f2be6f6958055711c72c0346d5f5
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2021-01-01 21:07:09 +0100

    [Lab 07] 05 - Show Marker for every stored Spot

--- app/src/main/java/com/archaeologicalfieldwork/activities/SpotMapsActivity.kt
+++ app/src/main/java/com/archaeologicalfieldwork/activities/SpotMapsActivity.kt
@@ -1,23 +1,46 @@
 package com.archaeologicalfieldwork.activities
 
+import android.app.Application
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.archaeologicalfieldwork.R
+import com.archaeologicalfieldwork.main.MainApp
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import kotlinx.android.synthetic.main.activity_spot_maps.*
 
 
 class SpotMapsActivity : AppCompatActivity() {
 
+    lateinit var app: MainApp
     lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        app = application as MainApp
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spot_maps)
         setSupportActionBar(toolbar)
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
 
+    }
+
+    fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.spots.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
     }
 
     override fun onDestroy() {
