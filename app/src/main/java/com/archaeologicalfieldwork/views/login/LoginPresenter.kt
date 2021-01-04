65ce7848a706fd8a4fe03b6bfc62537be06e99f4
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2021-01-04 17:13:17 +0100

    [Lab 10c] 07 - Implemented Login and signup with firebase

--- app/src/main/java/com/archaeologicalfieldwork/views/login/LoginPresenter.kt
+++ app/src/main/java/com/archaeologicalfieldwork/views/login/LoginPresenter.kt
@@ -3,14 +3,33 @@ package com.archaeologicalfieldwork.views.login
 import com.archaeologicalfieldwork.views.BasePresenter
 import com.archaeologicalfieldwork.views.BaseView
 import com.archaeologicalfieldwork.views.VIEW
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.toast
 
 class LoginPresenter(view: BaseView) : BasePresenter(view) {
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     fun doLogin(email: String, password: String) {
-        view?.navigateTo(VIEW.LIST)
+        view?.showProgress()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+            if (task.isSuccessful) {
+                view?.navigateTo(VIEW.LIST)
+            } else {
+                view?.toast("Login Failed: ${task.exception?.message}")
+            }
+            view?.hideProgress()
+        }
     }
 
     fun doSignUp(email: String, password: String) {
-        view?.navigateTo(VIEW.LIST)
+        view?.showProgress()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+            if (task.isSuccessful) {
+                view?.navigateTo(VIEW.LIST)
+            } else {
+                view?.toast("Sign Up Failed: ${task.exception?.message}")
+            }
+            view?.hideProgress()
+        }
     }
 }
