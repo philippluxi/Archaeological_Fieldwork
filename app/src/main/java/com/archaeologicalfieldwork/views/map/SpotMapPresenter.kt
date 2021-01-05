c7119a554025b00b4817c4b75c797f85c28bbe24
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2021-01-05 01:15:32 +0100

    [Bugfix] Cardview now displays selected marker

--- app/src/main/java/com/archaeologicalfieldwork/views/map/SpotMapPresenter.kt
+++ app/src/main/java/com/archaeologicalfieldwork/views/map/SpotMapPresenter.kt
@@ -18,16 +18,15 @@ class SpotMapPresenter(view: BaseView) : BasePresenter(view) {
         spots.forEach {
             val loc = LatLng(it.location.lat, it.location.lng)
             val options = MarkerOptions().title(it.title).position(loc)
-            map.addMarker(options).tag = it.id
+            map.addMarker(options).tag = it
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.location.zoom))
         }
     }
 
     fun doMarkerSelected(marker: Marker) {
-        val tag = marker.tag as Long
+        val spot = marker.tag as SpotModel
         doAsync {
-            val spot = app.spots.findById(tag)
-            uiThread {
+           uiThread {
                 if (spot != null) view?.showSpot(spot)
             }
         }
