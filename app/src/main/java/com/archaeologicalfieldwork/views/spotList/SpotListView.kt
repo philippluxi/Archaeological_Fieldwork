2ad345981b3e058e1c7875b5d419f40f359b2ba6
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2021-01-15 21:07:16 +0100

    [Feature] - First workin version of nav drawer

--- app/src/main/java/com/archaeologicalfieldwork/views/spotList/SpotListView.kt
+++ app/src/main/java/com/archaeologicalfieldwork/views/spotList/SpotListView.kt
@@ -2,25 +2,35 @@ package com.archaeologicalfieldwork.views.spotList
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.archaeologicalfieldwork.R
 import com.archaeologicalfieldwork.models.SpotModel
 import com.archaeologicalfieldwork.views.BaseView
 import kotlinx.android.synthetic.main.activity_spot_list.*
-import org.jetbrains.anko.toast
+import kotlinx.android.synthetic.main.drawer_list_layout.*
 
 class SpotListView : BaseView(), SpotListener {
 
     lateinit var presenter: SpotListPresenter
+    lateinit var navToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spot_list)
+
         super.init(toolbar, false)
+
+        navToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(navToggle)
+        navToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            navItemListener(it)
+        }
 
         presenter = initPresenter(SpotListPresenter(this)) as SpotListPresenter
 
@@ -41,10 +51,11 @@ class SpotListView : BaseView(), SpotListener {
         val searchView = menuItemSearch.getActionView() as SearchView
         searchView.maxWidth = Int.MAX_VALUE
 
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
+
             override fun onQueryTextChange(searchString: String?): Boolean {
                 presenter.loadSpotsForSearch(searchString as String)
                 return true
@@ -54,6 +65,9 @@ class SpotListView : BaseView(), SpotListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (navToggle.onOptionsItemSelected(item)) {
+            return true
+        }
         when (item.itemId) {
             R.id.item_add -> presenter.doaddSpot()
             R.id.item_map -> presenter.doShowSpotsMap()
@@ -64,6 +78,19 @@ class SpotListView : BaseView(), SpotListener {
             R.id.item_logout -> presenter.doLogout()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun navItemListener(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> presenter.doaddSpot()
+            R.id.item_map -> presenter.doShowSpotsMap()
+            R.id.item_all -> presenter.doShowAllSpots()
+            R.id.item_favorites -> presenter.doShowFavoriteSpots()
+            R.id.item_visited -> presenter.doShowVisitedSpots()
+            R.id.item_settings -> presenter.doShowSettings()
+            R.id.item_logout -> presenter.doLogout()
+        }
+        return true
     }
 
     override fun onSpotClick(spot: SpotModel) {
